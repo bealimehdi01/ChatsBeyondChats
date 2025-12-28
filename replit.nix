167ad33f5e177a0064bed917d1354fcec78a0f0b
Ali Mehdi <bealimehdi@gmail.com>
2025-12-28 22:25:48 +0530

    Add missing shared libraries for Puppeteer in Replit

--- replit.nix
+++ replit.nix
@@ -4,5 +4,25 @@
     pkgs.php82Packages.composer
     pkgs.sqlite
     pkgs.nodejs_20
+    pkgs.glib
+    pkgs.nss
+    pkgs.nspr
+    pkgs.at-spi2-atk
+    pkgs.pango
+    pkgs.cairo
+    pkgs.systemd
+    pkgs.dbus
+    pkgs.gtk3
+    pkgs.alsa-lib
+    pkgs.cups
+    pkgs.libdrm
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.mesa
+    pkgs.libxkbcommon
   ];
 }
